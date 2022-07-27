ef74e0afbceccc563e3c21b8a8e3c8f18e21bbdb
prabhat-cn <prabhat@capitalnumbers.com>
2022-07-28 00:35:14 +0530

    push

--- src/components/footer.js
+++ src/components/footer.js
@@ -19,86 +19,87 @@ import {
   IconLink,
   VisuallyHidden,
 } from "./ui"
-import BrandLogo from "./brand-logo"
+// import BrandLogo from "./brand-logo"
 
-const socialMedia = {
-  TWITTER: {
-    url: "https://twitter.com",
-    name: "Twitter",
-    icon: <Twitter />,
-  },
-  INSTAGRAM: {
-    url: "https://instagram.com",
-    name: "Instagram",
-    icon: <Instagram />,
-  },
-  FACEBOOK: {
-    url: "https://facebook.com",
-    name: "Facebook",
-    icon: <Facebook />,
-  },
-  YOUTUBE: {
-    url: "https://youtube.com",
-    name: "YouTube",
-    icon: <Youtube />,
-  },
-  GITHUB: {
-    url: "https://github.com",
-    name: "GitHub",
-    icon: <GitHub />,
-  },
-  TWITCH: {
-    url: "https://twitch.tv",
-    name: "Twitch",
-    icon: <Twitch />,
-  },
-}
+// const socialMedia = {
+//   TWITTER: {
+//     url: "https://twitter.com",
+//     name: "Twitter",
+//     icon: <Twitter />,
+//   },
+//   INSTAGRAM: {
+//     url: "https://instagram.com",
+//     name: "Instagram",
+//     icon: <Instagram />,
+//   },
+//   FACEBOOK: {
+//     url: "https://facebook.com",
+//     name: "Facebook",
+//     icon: <Facebook />,
+//   },
+//   YOUTUBE: {
+//     url: "https://youtube.com",
+//     name: "YouTube",
+//     icon: <Youtube />,
+//   },
+//   GITHUB: {
+//     url: "https://github.com",
+//     name: "GitHub",
+//     icon: <GitHub />,
+//   },
+//   TWITCH: {
+//     url: "https://twitch.tv",
+//     name: "Twitch",
+//     icon: <Twitch />,
+//   },
+// }
 
-const getSocialURL = ({ service, username }) => {
-  const domain = socialMedia[service]?.url
-  if (!domain) return false
-  return `${domain}/${username}`
-}
+// const getSocialURL = ({ service, username }) => {
+//   const domain = socialMedia[service]?.url
+//   if (!domain) return false
+//   return `${domain}/${username}`
+// }
 
-const getSocialIcon = ({ service }) => {
-  return socialMedia[service]?.icon
-}
+// const getSocialIcon = ({ service }) => {
+//   return socialMedia[service]?.icon
+// }
 
-const getSocialName = ({ service }) => {
-  return socialMedia[service]?.name
-}
+// const getSocialName = ({ service }) => {
+//   return socialMedia[service]?.name
+// }
 
 export default function Footer() {
-  const data = useStaticQuery(graphql`
-    query {
-      layout {
-        footer {
-          id
-          links {
-            id
-            href
-            text
-          }
-          meta {
-            id
-            href
-            text
-          }
-          copyright
-          socialLinks {
-            id
-            service
-            username
-          }
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     layout {
+  //       footer {
+  //         id
+  //         links {
+  //           id
+  //           href
+  //           text
+  //         }
+  //         meta {
+  //           id
+  //           href
+  //           text
+  //         }
+  //         copyright
+  //         socialLinks {
+  //           id
+  //           service
+  //           username
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
 
-  const { links, meta, socialLinks, copyright } = data.layout.footer
+  // const { links, meta, socialLinks, copyright } = data.layout.footer
 
   return (
-    <Box as="footer" paddingY={4}>
+    <>
+      {/* <Box as="footer" paddingY={4}>
       <Container>
         <Flex variant="start" responsive>
           <NavLink to="/">
@@ -148,6 +149,7 @@ export default function Footer() {
         </Flex>
       </Container>
       <Space size={3} />
-    </Box>
+    </Box> */}
+    </>
   )
 }
